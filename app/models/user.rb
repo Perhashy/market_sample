3a2bcc2865008c0485e2ac54be9375f4894b95f9
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-24 17:59:40 +0900

    全角制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
-
+  validates :nickname, :birthday, presence: true
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/}
   has_many :addresses
 
   # validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/,
